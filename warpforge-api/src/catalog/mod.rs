99de989480621dd6c9669c2afb7fc7027288fbdd
Eric Myhre <hash@exultant.us>
2023-06-05 16:45:44 +0200

    Name some API strings with newtype pattern.  Comments on validation approaches to research.

--- warpforge-api/src/catalog/mod.rs
+++ warpforge-api/src/catalog/mod.rs
@@ -17,9 +17,33 @@ pub enum CatalogModuleCapsule {
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct CatalogModule {
     name: String, // TODO figure out how to wrap this better.  "newtype" pattern?
-    releases: IndexMap<String, String>, // TODO ReleaseName, some type for CIDs
+    releases: IndexMap<ReleaseName, String>, // TODO some type for CIDs?  Or should we just leave these as opaque strings at this level?  ... let's have a CID.  Strict seems right in this spot.
     metadata: IndexMap<String, String>, // Actually really is just strings :) // FUTURE: I yet don't know how to do "any" with serde in a codec-agnostic way, if we did want to.
 }
+
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+struct ReleaseName(String); // Does not currently accomplish anything other than naming and documentation.  FUTURE: some validation rules would be nice -- see comments below about how, though.
+
+/*
+About validation:
+
+- Ideally, Serde knows about validation... because that means if it fails, the error comes out with line info!  That's a big deal.
+- But, becoming unable to handle data that doesn't validate is annoying in other scenarios.  So, I'd probably also want to be able to disable having serde do validation sometimes, to be able to passthrough values without guff.
+- I like the ideas espoused in https://lexi-lambda.github.io/blog/2019/11/05/parse-don-t-validate/ , particular to use the type system to remember if something is validated or not...
+  - ... but ISTM you need some language-level support for the concept of refinement types in order to really make that viable on structures with any degree of nesting of types.  Otherwise you're writing a LOT of near-duplicate types; it's a taint effect -- it spreads to everything containing it.
+    - And if that's possible in Rust, I don't know how, yet.
+
+I flirt with the idea of adding non-serialized fields that track whether something _did_ pass validation.  That lets one have a nice middle ground... *but*, it doesn't solve the Serde-halts-with-line-info desire.
+
+Overall if we come to "pick one", the most useful is probably to validate during serde and at all times.
+
+Note that we're also rife with things that aren't appropriate to validate during parsing.
+For example:
+
+- connectedness of graphs when using pipe syntax in actions: that's a whole-document check, so there's no value (nor real possibility) of doing it with during the first pass and emitting info with line numbers.
+- whether some step is using mount and ingest features or not -- we _could_ turn that into a validate thing, and it might be nice to do so, but it has the type-explosion problem again.  Unless we can figure out refinement types or way to make validation conditional.
+*/
 
 #[cfg(test)]
 mod tests {
