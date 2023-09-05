1e39e1be9a39466f8246e446b325ebd5b0ab0cf4
Eric Myhre <hash@exultant.us>
2023-09-05 15:08:22 +0200

    drop another level of indentation by admitting this whole block is purely a match

--- warpforge-cli/src/main.rs
+++ warpforge-cli/src/main.rs
@@ -9,8 +9,13 @@ mod errors;
 use errors::*;
 
 fn main() {
-	main2().expect("wow")
-	// TODO: destructure the enum members into distinct exit codes.
+	match main2() {
+		Ok(_) => {}
+		Err(e) => {
+			println!("{}", e);
+			std::process::exit(10); // FIXME select codes per error variant
+		}
+	}
 }
 
 fn main2() -> Result<(), Error> {
@@ -27,38 +32,36 @@ fn main2() -> Result<(), Error> {
 	//   - 1: to receive the command object with all parents.
 	//   - 2: to have a func on my command strugs that receives a call, rather than have to make this dispatch table.
 	match &cli.subcommand {
-		Some(cmds::Subcommands::Catalog(cmd)) => {
-			match &cmd.subcommand {
-				cmds::catalog::Subcommands::ReadItem(cmd) => {
-					let user_home = env::var("HOME")
-						.map_err(|e| Error::BizarreEnvironment { cause: Box::new(e) })?;
+		Some(cmds::Subcommands::Catalog(cmd)) => match &cmd.subcommand {
+			cmds::catalog::Subcommands::ReadItem(cmd) => {
+				let user_home = env::var("HOME")
+					.map_err(|e| Error::BizarreEnvironment { cause: Box::new(e) })?;
 
-					// Create the catalog data access broker.  Store in a box just so we can have dynamic dispatch.  (This is architecture astronauting, but I wanna know that I know how to do this.)
-					//TODO: check for a root workspace above $CWD before $HOME/.warphome
-					let catalog_handle: Box<dyn dab::catalog::Handle> =
-						Box::new(dab::catalog::FsHandle::new(
-							path::Path::new(&user_home).join(".warphome/catalogs/warpsys"),
-						));
+				// Create the catalog data access broker.  Store in a box just so we can have dynamic dispatch.  (This is architecture astronauting, but I wanna know that I know how to do this.)
+				//TODO: check for a root workspace above $CWD before $HOME/.warphome
+				let catalog_handle: Box<dyn dab::catalog::Handle> =
+					Box::new(dab::catalog::FsHandle::new(
+						path::Path::new(&user_home).join(".warphome/catalogs/warpsys"),
+					));
 
-					let catalog_release = catalog_handle
-						.load_release(&cmd.catalog_ref.module_name, &cmd.catalog_ref.release_name)
-						.map_err(|e| Error::CatalogAccessError { cause: e })?;
+				let catalog_release = catalog_handle
+					.load_release(&cmd.catalog_ref.module_name, &cmd.catalog_ref.release_name)
+					.map_err(|e| Error::CatalogAccessError { cause: e })?;
 
-					match catalog_release.items.get(&cmd.catalog_ref.item_name) {
-						Some(wareid) => {
-							println!("{wareid}");
-							Ok(())
-						}
-						None => {
-							println!("catalog item not found.");
-							Err(Error::CatalogEntryNotExists {
-								reference: cmd.catalog_ref.clone(),
-							})
-						}
+				match catalog_release.items.get(&cmd.catalog_ref.item_name) {
+					Some(wareid) => {
+						println!("{wareid}");
+						Ok(())
+					}
+					None => {
+						println!("catalog item not found.");
+						Err(Error::CatalogEntryNotExists {
+							reference: cmd.catalog_ref.clone(),
+						})
 					}
 				}
 			}
-		}
+		},
 		Some(cmds::Subcommands::Ware(cmd)) => match &cmd.subcommand {
 			cmds::ware::Subcommands::Unpack(cmd) => {
 				use std::io::{BufRead, BufReader};
